48d9ff0711562e9b862d1fd6e679218f8ceda4dd
Alexander Whitney <awhitney@squareup.com>
2020-12-08 23:26:46 -0500

    add some utility functions

--- lib/day.ts
+++ lib/day.ts
@@ -1,3 +1,5 @@
+import * as _ from 'lodash';
+
 import {readFileSync} from 'fs';
 
 export abstract class Day {
@@ -18,13 +20,42 @@ export abstract class Day {
     run() {
         this.initialize();
 
-        console.log('*** PART 1 ***');
+        this.print('*** PART 1 ***');
         let result = this.executePart1();
-        console.log('Result: ' + result);
+        this.print(`Result: ${result}`);
 
-        console.log();
-        console.log('*** PART 2 ***');
+        this.print('\n*** PART 2 ***');
         result = this.executePart2();
-        console.log('Result: ' + result);
+        this.print(`Result: ${result}`);
+    }
+
+    /**
+     * Because console.log() is too long to type
+     */
+    protected print(...things: any) {
+        console.log.call(console, ...things);
+    }
+
+    /**
+     * Replaces all occurrences of the target string with the replacement string in str.
+     * 
+     * Because str.replace(new RegExp(target, 'g'), replacement) is cumbersome and
+     * string.prototype.replaceAll is not available yet
+     */
+    protected sReplAll(str: string, target: string, replacement: string): string {
+        return str.replace(new RegExp(target, 'g'), replacement);
+    }
+
+    /**
+     * Converts a string consisting of two characters/strings into binary
+     * @param str the string
+     * @param oneValue The character/string representing 1
+     * @param zeroValue The character/string representing 0
+     */
+    protected sToBinary(str: string, oneValue: string, zeroValue: string): number {
+        if (_.uniq(str).length > 2) throw new Error(`"${str}" does not appear to be a ${oneValue}${zeroValue} string`);
+        str = this.sReplAll(str, oneValue, "1");
+        str = this.sReplAll(str, zeroValue, "0");
+        return parseInt(str, 2);
     }
 }

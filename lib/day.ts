e2187a2d7054479dac14309e21d1d25b78b6b7f0
Alex Whitney <alex@itshalfempty.com>
2020-12-19 23:17:34 -0500

    add timings for running each part

--- lib/day.ts
+++ lib/day.ts
@@ -1,4 +1,5 @@
 import * as _ from 'lodash';
+import * as moment from 'moment';
 
 import * as u from './util';
 import {readFileSync} from 'fs';
@@ -19,14 +20,21 @@ export abstract class Day {
     abstract executePart2(): string|number;
 
     run() {
+        u.print('Initializing...');
+        let start = moment();
         this.initialize();
+        u.print(`Completed in ${moment().diff(start)}ms`);
 
-        u.print('*** PART 1 ***');
+        u.print('\n*** PART 1 ***');
+        start = moment();
         let result = this.executePart1();
+        u.print(`Completed in ${moment().diff(start)}ms`);
         u.print(`Result: ${result+""}`);
 
         u.print('\n*** PART 2 ***');
+        start = moment();
         result = this.executePart2();
+        u.print(`Completed in ${moment().diff(start)}ms`);
         u.print(`Result: ${result+""}`);
     }
 
